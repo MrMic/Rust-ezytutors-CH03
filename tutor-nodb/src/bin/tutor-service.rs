787e04ceb01d87dc786178b245be00e30ab67ccf
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-07 14:50:44 +0200

    feat(UX): Defining & Managing application State

---
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -0,0 +1,31 @@
+use actix_web::{web, App, HttpServer};
+use std::{io, sync::Mutex};
+
+use routes::*;
+use state::AppState;
+
+// ______________________________________________________________________
+#[path = "../handlers.rs"]
+mod handlers;
+
+#[path = "../routes.rs"]
+mod routes;
+
+#[path = "../state.rs"]
+mod state;
+// ______________________________________________________________________
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already asked me 'I'm good!'".to_string(),
+        visit_count: Mutex::new(0),
+    });
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+    };
+
+    HttpServer::new(app).bind("127.0.0.1:3002")?.run().await
+}
