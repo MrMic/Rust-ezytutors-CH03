19b9ed16c2a8c80793ed4b74cad4421eb2d98ea4
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-11 14:57:31 +0200

    feat(UX): Handler Function

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -1,7 +1,9 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 
-use crate::state::AppState;
+use crate::{models::Course, state::AppState};
 
+// ______________________________________________________________________
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;
     let mut visit_count = app_state.visit_count.lock().unwrap();
@@ -9,4 +11,32 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     *visit_count += 1;
 
     HttpResponse::Ok().json(&response)
+}
+
+// ______________________________________________________________________
+pub async fn new_course(
+    // INFO: ++ Extract Data Payload from HTTP Request +------------------------+
+    new_course: web::Json<Course>,
+    // INFO: ++ Extract App State from HTTP Request +------------------------+
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count_for_user = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.tutor_id == new_course.tutor_id)
+        .count();
+
+    let new_course = Course {
+        tutor_id: new_course.tutor_id,
+        course_id: Some((course_count_for_user + 1) as i32),
+        course_name: new_course.course_name.clone(),
+        posted_time: Some(Utc::now().naive_utc()),
+    };
+
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Added new course")
 }
