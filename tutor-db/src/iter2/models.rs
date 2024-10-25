9d26faeb293efca2606122f1fe4224f4eda3882a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-25 14:52:17 +0200

    feat(UX): set up connection pool w/ dependency injection

--- tutor-db/src/iter2/models.rs
+++ tutor-db/src/iter2/models.rs
@@ -1,6 +1,8 @@
 use actix_web::web;
 use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Course {
     pub course_id: i32,
     pub tutor_id: i32,
