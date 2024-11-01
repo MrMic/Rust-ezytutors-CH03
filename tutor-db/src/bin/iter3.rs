485f380d18052422ab146c5af69d596d9529c8bf
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-11-01 14:51:54 +0100

    feat: write DB access functions

---
+++ tutor-db/src/bin/iter3.rs
@@ -0,0 +1,15 @@
+#[path = "../iter3/db_access.rs"]
+mod db_access;
+#[path = "../iter3/handlers.rs"]
+mod handlers;
+#[path = "../iter3/models.rs"]
+mod models;
+#[path = "../iter3/routes.rs"]
+mod routes;
+#[path = "../iter3/state.rs"]
+mod state;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    todo!()
+}
