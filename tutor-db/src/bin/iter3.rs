f3c7af8c93ef8c7a9b57a0f95470bfb9f5f70b54
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-11-14 21:28:57 +0100

    feat: create & query records from the DB

--- tutor-db/src/bin/iter3.rs
+++ tutor-db/src/bin/iter3.rs
@@ -1,3 +1,12 @@
+use actix_web::{web, App};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use state::AppState;
+use std::{env, sync::Mutex};
+
+use routes::*;
+
+// ______________________________________________________________________
 #[path = "../iter3/db_access.rs"]
 mod db_access;
 #[path = "../iter3/handlers.rs"]
@@ -9,7 +18,33 @@ mod routes;
 #[path = "../iter3/state.rs"]
 mod state;
 
+// ╾────────────────────────────────────────────────────────────────────╼
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    todo!()
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You have already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+
+    let app = move || {
+        let shared_data = shared_data.clone();
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+
+    // * INFO: Start HTTP Server
+    println!("Starting HTTP server: 127.0.0.1:3002");
+
+    actix_web::HttpServer::new(app)
+        .bind("127.0.0.1:3002")?
+        .run()
+        .await
 }
