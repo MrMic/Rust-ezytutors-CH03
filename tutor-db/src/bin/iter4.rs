13181a551121e0def48c94cfe09bf3138e96d11c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-11-18 15:18:39 +0100

    feat: Setup project - iter4 -

---
+++ tutor-db/src/bin/iter4.rs
@@ -0,0 +1,52 @@
+use actix_web::{web, App};
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use state::AppState;
+use std::{env, sync::Mutex};
+
+use routes::*;
+
+// ______________________________________________________________________
+#[path = "../iter4/db_access.rs"]
+mod db_access;
+#[path = "../iter4/handlers.rs"]
+mod handlers;
+#[path = "../iter4/models.rs"]
+mod models;
+#[path = "../iter4/routes.rs"]
+mod routes;
+#[path = "../iter4/state.rs"]
+mod state;
+
+// ╾────────────────────────────────────────────────────────────────────╼
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You have already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+
+    let app = move || {
+        let shared_data = shared_data.clone();
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+
+    // * INFO: Start HTTP Server
+    println!("Starting HTTP server: 127.0.0.1:3002");
+
+    let host_port = env::var("HOST_PORT").expect("HOST:PORT is not set in .env file");
+
+    actix_web::HttpServer::new(app)
+        .bind(&host_port)?
+        .run()
+        .await
+}
--- tutor-db/src/database.sql
+++ tutor-db/src/database.sql
@@ -1,9 +1,9 @@
 /* Drop table if already existe */
-DROP TABLE IF EXISTS ezy_course_c4;
+DROP TABLE IF EXISTS ezy_course_c5;
 
 /* Create table */
 /* Don't put a comma after the last field */
-CREATE TABLE ezy_course_c4
+CREATE TABLE ezy_course_c5
 (
   course_id serial primary key,
   tutor_id int NOT NULL,
@@ -12,9 +12,9 @@ CREATE TABLE ezy_course_c4
 );
 
 /* Load seed data for testing */
-insert into ezy_course_c4 
+insert into ezy_course_c5 
   (course_id, tutor_id, course_name, posted_time) 
-values (1, 1, 'First course', '2020-12-17 05:40:00');
-insert into ezy_course_c4 
+values (1, 1, 'First course', '2021-03-17 05:40:00');
+insert into ezy_course_c5 
   (course_id, tutor_id, course_name, posted_time) 
-values (2, 1, 'Second course', '2020-12-18 05:45:00');
+values (2, 1, 'Second course', '2021-03-18 05:45:00');
